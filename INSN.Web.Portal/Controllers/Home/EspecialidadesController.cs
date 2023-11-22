a215f7e7f0226ea3976f158375b9f7986d71ffcb
sinformatica <sinformatica@insn.gob.pe>
2023-11-22 13:39:06 -0500

    c
    c

--- INSN.Web.Portal/Controllers/Home/EspecialidadesController.cs
+++ INSN.Web.Portal/Controllers/Home/EspecialidadesController.cs
@@ -24,6 +24,40 @@ namespace INSN.Web.Portal.Controllers.Home
             return View("~/Views/Home/Especialidades/Cardiologia.cshtml");
         }
 
+        public IActionResult AlergiaAsmaInmunologia()
+        {
+            return View("~/Views/Home/Especialidades/AlergiaAsmaInmunologia.cshtml");
+        }
+        
+        public IActionResult Gastroenterologia()
+        {
+            return View("~/Views/Home/Especialidades/Gastroenterologia.cshtml");
+        }
+        
+        public IActionResult Dermatologia()
+        {
+            return View("~/Views/Home/Especialidades/Dermatologia.cshtml");
+        }
+
+        public IActionResult Endocrinologia()
+        {
+            return View("~/Views/Home/Especialidades/Endocrinologia.cshtml");
+        }
+        
+        public IActionResult Hematologia()
+        {
+            return View("~/Views/Home/Especialidades/Hematologia.cshtml");
+        }
+
+        public IActionResult Infectologia()
+        {
+            return View("~/Views/Home/Especialidades/Infectologia.cshtml");
+        }
+
+        public IActionResult MedicinaAdolescente()
+        {
+            return View("~/Views/Home/Especialidades/MedicinaAdolescente.cshtml");
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
