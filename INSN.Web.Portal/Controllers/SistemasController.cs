47c0be993156e68ed598378b398e81ad30b36d17
sinformatica <sinformatica@insn.gob.pe>
2023-12-26 12:17:40 -0500

    sistemas

--- INSN.Web.Portal/Controllers/SistemasController.cs
+++ INSN.Web.Portal/Controllers/SistemasController.cs
@@ -9,19 +9,19 @@ namespace INSN.Web.Portal.Controllers
     {
         private readonly ISistemasProxy _proxy;
 
-        //public IActionResult Index()
-        //{
-        //    return View("~/Views/User/Sistemas.cshtml");
-        //}
-
-        public async Task<IActionResult> Index(LoginViewModel model)
+        public IActionResult Index()
         {
-            var response = await _proxy.ListarSistemasPorUsuario(new LoginUsuarioDtoRequest()
-            {
-                Usuario = model.Usuario
-            });
-
-            return View("~/Views/User/Sistemas.cshtml", model);
+            return View("~/Views/User/Sistemas.cshtml");
         }
+
+        //public async Task<IActionResult> Index(LoginViewModel model)
+        //{
+        //    var response = await _proxy.ListarSistemasPorUsuario(new LoginUsuarioDtoRequest()
+        //    {
+        //        Usuario = model.Usuario
+        //    });
+
+        //    return View("~/Views/User/Sistemas.cshtml", model);
+        //}
     }
 }
