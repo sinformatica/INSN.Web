56205a2febf6e3f7c4eb3e41e93dfc218252b064
sinformatica <sinformatica@insn.gob.pe>
2024-01-25 09:06:54 -0500

    commit

---
+++ INSN.Web.ViewModels/Common/Constantes/Enumerado.cs
@@ -0,0 +1,60 @@
+﻿namespace INSN.Web.ViewModels.Common.Constantes
+{
+    /// <summary>
+    /// Variables - Constantes - Enumerado
+    /// </summary>
+    public class Enumerado
+    {
+        /// <summary>
+        /// Constantes Tipo Convocatoria Id
+        /// </summary>
+        public struct TipoConvocatoriaId
+        {
+            /// <summary>
+            /// Id Cas
+            /// </summary>
+            public const int Cas = 1;
+
+            /// <summary>
+            /// Id Interno
+            /// </summary>
+            public const int Interno = 2;
+
+            /// <summary>
+            /// Id Público
+            /// </summary>
+            public const int Público = 3;
+
+            /// <summary>
+            /// Id Suplencias
+            /// </summary>
+            public const int Suplencias = 4;
+
+            /// <summary>
+            /// Id Nombramiento
+            /// </summary>
+            public const int Nombramiento = 5;
+
+            /// <summary>
+            /// Id Reasignación
+            /// </summary>
+            public const int Reasignación = 6;
+        }
+
+        /// <summary>
+        /// Constantes Tipo Convocatoria Id
+        /// </summary>
+        public struct EstadoConvocatoria
+        {
+            /// <summary>
+            /// En Proceso
+            /// </summary>
+            public const string EnProceso = "E";
+
+            /// <summary>
+            /// Concluido
+            /// </summary>
+            public const string Concluido = "C";
+        }
+    }
+}
