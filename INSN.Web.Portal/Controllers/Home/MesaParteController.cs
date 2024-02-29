f75a0e1f200ef47ba47761d462a59dc02fab9279
sinformatica <sinformatica@insn.gob.pe>
2024-02-29 14:39:35 -0500

    mesa de partes jr

--- INSN.Web.Portal/Controllers/Home/MesaParteController.cs
+++ INSN.Web.Portal/Controllers/Home/MesaParteController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using INSN.Utilitarios;
+using Microsoft.AspNetCore.Mvc;
 
 namespace INSN.Web.Portal.Controllers.Home;
 
@@ -22,4 +23,34 @@ public class MesaParteController : Controller
     {
         return View("~/Views/Home/MesaParte/Index.cshtml");
     }
+
+    #region[Abrir archivo]
+    /// <summary>
+    /// Adaptador Abrir Archivo
+    /// </summary>
+    /// <param name="RutaArchivo"></param>
+    /// <returns></returns>
+    public IActionResult AdaptadorAbrirArchivo(string RutaArchivo)
+    {
+        try
+        {
+            // Biblioteca Utilitarios
+            var (contenidoArchivo, tipoContenido) = GestorArchivo.ObtenerContenidoArchivo(RutaArchivo);
+
+            return File(contenidoArchivo, tipoContenido);
+        }
+        catch (ArgumentException)
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+        catch (FileNotFoundException)
+        {
+            return View("~/Views/Shared/NotFound.cshtml");
+        }
+        catch (Exception)
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+    #endregion
 }
